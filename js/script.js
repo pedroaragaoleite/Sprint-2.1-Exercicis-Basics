82bf29690a124337cf332e475622d9b25b650cc2
Pedro Aragão <pedro.aragao.leite@gmail.com>
2024-01-31 15:23:12 +0100

    bloco 1.3 update

--- js/script.js
+++ js/script.js
@@ -118,3 +118,79 @@ const determinarNum = (num) => {
 const trombarMaxim = (a, b, c) => {
     return a > b && a > c ? `El ${a} es mayor que ${b} y el ${c}` : b > a && b > c ? `El ${b} es mayor que ${a} y el ${c}` : `El ${c} es mayor que ${a} y el ${b}`; 
 }
+
+
+// Nivell 3
+
+// Exercici 4
+// Operador ternari dins un bucle: 
+// Escriu una funció parOImpar que accepti un array de números i utilitzi un bucle per a recórrer l'array.
+// Dins del bucle, utilitza l'operador ternari per a determinar si cada número és parell o imparell.
+
+const numsParImpar = [1,2,3,4,5,6,7,8,9,10];
+
+const parOImpar = (array) => {
+    for(let i = 0; i < array.length; i++) {
+        const num = array[i];
+        console.log(num % 2 === 0 ? `El número ${num} es par` : `El número ${num} és impar`);
+    }
+}
+
+// Bloc 1.3: Callbacks
+
+// Nivell 1
+
+//Exercici 1
+//Callback bàsic: 
+//Escriu una funció anomenada processar que accepti dos paràmetres:
+// un nombre i una funció de callback. La funció processar ha d'invocar la funció de callback, passant el nombre com a paràmetre.
+
+function processar(nombre, callback) {
+    let miNombre = `Tu nombre és: ${nombre}`;
+    callback(miNombre);
+}
+
+function nombreProprio(nombre) {
+    console.log(nombre);
+}
+
+const proc = (nombre, callback) => {
+    let miNombre = `Tu nombre és: ${nombre}`;
+    callback(miNombre);
+}
+
+processar("pedro", nombreProprio);
+proc("Juno", nombreProprio);
+
+
+// Exercici 2
+// Callbacks amb operacions matemàtiques: 
+// Escriu una funció calculadora que accepti tres paràmetres: 
+//dos nombres i una funció de callback. 
+//La funció calculadora ha d'invocar la funció de callback amb els dos nombres com a paràmetres. 
+// Després, crida calculadora amb una funció que faci la suma dels dos nombres.
+
+function calculadora(num1, num2, callback) {
+    callback(num1, num2);
+}
+
+function sumar(num1, num2) {
+    console.log(num1 + num2);
+}
+
+calculadora(2, 2, sumar);
+
+// Nivell 2
+
+
+//Exercici 3
+//Ús de callbacks en funcions asíncrones:
+// Escriu una funció esperarISaludar que accepti dos paràmetres: un nom i una funció de callback.
+// La funció ha d'esperar 2 segons i llavors invocar la funció de callback, passant el nom com a paràmetre.
+
+
+
+
+//Exercici 4
+//Callbacks amb arrays: Escriu una funció processarElements que accepti dos paràmetres: un array i una funció de callback. 
+//La funció processarElements ha d'invocar la funció de callback per cada element de l'array.
